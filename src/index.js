370f538f021e6af94b46665dd9f0be28f02d8a84
Bogdan Salyp <bogdansalyp@gmail.com>
2019-09-04 00:08:17 +0300

    ESLint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const component = () => {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "<h1>Hello World!</h1>";
+    element.innerHTML = '<h1>Hello World!</h1>';
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
             {
                 test: /\.(js)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: ['babel-loader', 'eslint-loader']
             }
         ]
     },
