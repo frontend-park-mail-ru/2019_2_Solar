ff9b7efe7625e908b4dd10bb36ed66873fe1b6c4
Nell <ADshishova@mail.ru>
2019-11-04 12:40:12 +0300

    deleted dialog from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,6 @@
 import './Header.scss';
 import HeaderTemplate from '../Header/Header.hbs';
 
-import Dialog1ViewComponent from '../DialogComponent/Dialog1ViewComponent/Dialog1ViewComponent.js';
-import Dialog3ViewComponent from '../DialogComponent/Dialog3ViewComponent/Dialog3ViewComponent.js';
-import MessageComponent from '../Message/Message.js';
-
 import bus from '../../utils/bus.js';
 
 import Logo from '../../images/logo.png';
@@ -66,7 +62,7 @@ export default class HeaderComponent {
         };
         const html = HeaderTemplate(context);
 
-        this._parent.innerHTML += html;
+        this._parent.innerHTML = html;
 
         /* for picture logo*/
         const toIndex = document.getElementById('header').querySelectorAll('[data-section=\'index\']')[0];
@@ -87,47 +83,6 @@ export default class HeaderComponent {
         toSettings.addEventListener('click', (e) => {
             e.preventDefault();
             bus.emit('/settings');
-        });
-
-        /* Заглушка для отображения диалога */
-        const messageView = document.getElementById('dialogview-page');
-        messageView.className = 'dialogview__flex-container';
-        let messageViewIsOpen = false;
-
-        const messageImg = document.getElementById('messageImg');
-        messageImg.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            if (messageViewIsOpen == false) {
-                const dialog1 = new Dialog1ViewComponent(messageView);
-                messageView.className = 'dialogview__flex-container_height';
-                dialog1.render();
-                messageViewIsOpen = true;
-
-                const newMessage = document.getElementById('newMessagwButton');
-                newMessage.addEventListener('click', (e)=> {
-                    messageView.innerHTML = '';
-                    const dialog3 = new Dialog3ViewComponent(messageView);
-                    dialog3.render();
-
-                    const messageViewList = document.getElementById('MessagesList');
-
-                    const createMessageForm = document.getElementById('createMessageData');
-                    createMessageForm.addEventListener('submit', (e) => {
-                        e.preventDefault();
-
-                        const message = createMessageForm.elements['message'].value;
-                        const newMessage = new MessageComponent(messageViewList);
-                        newMessage.render({messageAuthor: this._data.body.user.username, messageContent: message});
-
-                        createMessageForm.elements['message'].value = '';
-                    });
-                });
-            } else {
-                messageView.innerHTML = '';
-                messageView.className = 'dialogview__flex-container';
-                messageViewIsOpen = false;
-            }
         });
     }
 }
