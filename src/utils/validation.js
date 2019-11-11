d4412ab22d9fa3a6d9bdd349382745f8a8fb4f85
Nell <ADshishova@mail.ru>
2019-11-12 01:38:49 +0300

    fixed validation (added message and fields)

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -55,7 +55,58 @@ export function validateStatus(value) {
  * @return {boolean} Valid or not.
  */
 export function validateSignup(signUpForm) {
-    return (validateEmail(signUpForm.elements['email'].value) &&
-            validatePassword(signUpForm.elements['password'].value) &&
-            validateUsername(signUpForm.elements['username'].value));
+    const errorList = {
+        errorMessage: '',
+        errorNumbers: [],
+    };
+
+    if (!validateEmail(signUpForm.elements['email'].value)) {
+        errorList.errorMessage += 'емейл, ';
+        errorList.errorNumbers.push('email');
+    }
+
+    if (!validatePassword(signUpForm.elements['password'].value)) {
+        errorList.errorMessage += 'пароль, ';
+        errorList.errorNumbers.push('password');
+    }
+
+    if (!validateUsername(signUpForm.elements['username'].value)) {
+        errorList.errorMessage += 'никнейм, ';
+        errorList.errorNumbers.push('username');
+    }
+
+    if (errorList.errorMessage.length > 0) {
+        errorList.errorMessage = errorList.errorMessage.slice(0, -2);
+    }
+
+    if (errorList.errorMessage.length > 0 && errorList.errorNumbers.length > 0) {
+        return errorList;
+    }
+
+    return null;
+}
+
+/**
+ * error Draw
+ * @param {*} listWithFields
+ * @param {*} validationErrorNumbers
+ * @param {*} className
+ */
+export function errorDraw(listWithFields, validationErrorNumbers, className) {
+    for (let i = 0; i < validationErrorNumbers.length; i++) {
+        listWithFields[validationErrorNumbers[i]].classList.add(className);
+    }
+}
+
+/**
+ * Remove error style
+ * @param {*} listWithFields
+ * @param {*} className
+ */
+export function deleteErrorDraw(listWithFields, className) {
+    for (const key in listWithFields) {
+        if (listWithFields.hasOwnProperty(key)) {
+            listWithFields[key].classList.remove(className);
+        }
+    }
 }
