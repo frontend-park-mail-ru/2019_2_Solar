043cb0c099f3ea7bc748723e7c9f5123bd9e9944
Nell <ADshishova@mail.ru>
2019-11-16 01:44:11 +0300

    Auto stash before merge of "dev_fetch" and "origin/dev"

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,2 +1,2 @@
-export const BACKEND_ADDRESS = 'http://solarsunrise.ru:8080';
-// export const BACKEND_ADDRESS = 'http://localhost:8080';
+// export const BACKEND_ADDRESS = 'http://167.172.183.157:8080';
+export const BACKEND_ADDRESS = 'http://localhost:8080';
