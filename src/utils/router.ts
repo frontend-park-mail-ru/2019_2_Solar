6f5bea72a0b4c123f8401d60a3f19e869de079b2
Nell <ADshishova@mail.ru>
2019-12-12 23:53:29 +0300

    fixed router

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -110,25 +110,17 @@ export default class Router {
      */
     start() {
         this.root.addEventListener('click', function(event) {
-            if (!(event.target instanceof HTMLAnchorElement)) {
+            if (!(event.target instanceof HTMLAnchorElement || event.target instanceof HTMLImageElement)) {
                 return;
-            }
-
-            event.preventDefault();
-            const link = event.target;
-
-            this.open(link.pathname);
-
-            // if (!(event.target instanceof HTMLAnchorElement || event.target instanceof HTMLImageElement)) {
-            //     return;
-            // } else if (event.target instanceof HTMLAnchorElement) {
-            //     event.preventDefault();
-            //     const link = event.target;
+            } else if (event.target instanceof HTMLAnchorElement) {
+                event.preventDefault();
+                const link = event.target;
     
-            //     this.open(link.pathname);
-            // } else if (event.target instanceof HTMLImageElement) {
-            //     this.open(event.target.dataset.section);
-            // }
+                this.open(link.pathname);
+            } else if (event.target instanceof HTMLImageElement) {
+                event.preventDefault();
+                this.open(event.target.dataset.section);
+            }
         }.bind(this));
 
         window.addEventListener('popstate', function() {
