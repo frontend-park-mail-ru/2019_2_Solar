de227f5fcbfad5fef85b9b4ec1819c059a089d0d
Bogdan Salyp <bogdansalyp@gmail.com>
2019-12-12 14:14:15 +0000

    Fixed https

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_ADDRESS = 'http://solarsunrise.ru:8080';
-export const WS_BACKEND_ADDRESS = 'ws://solarsunrise.ru:8080';
+export const BACKEND_ADDRESS = 'https://solarsunrise.ru:8080';
+export const WS_BACKEND_ADDRESS = 'wss://solarsunrise.ru:8080';
 // export const WS_BACKEND_ADDRESS = 'ws://localhost:8080';
 // export const BACKEND_ADDRESS = 'http://localhost:8080';
