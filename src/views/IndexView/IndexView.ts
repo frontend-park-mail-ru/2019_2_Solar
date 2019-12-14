07f1355d11b708f19d25e8ad25e7819455c0819b
Nell <ADshishova@mail.ru>
2019-12-14 20:34:22 +0300

    endless scroll

--- src/views/IndexView/IndexView.ts
+++ src/views/IndexView/IndexView.ts
@@ -42,7 +42,7 @@ export default class IndexView extends BaseView {
                 (<any>window).CSRFtoken = responseBody.csrf_token;
 
                 fetchModule.Get({
-                    url: BACKEND_ADDRESS + '/pin/list/' + this.args,
+                    url: BACKEND_ADDRESS + '/pin/list/' + this.args + '?limit=10&id=9',
                     body: null,
                 })
                     .then((response) => {
@@ -75,7 +75,61 @@ export default class IndexView extends BaseView {
                         } else {
                             indexPage.textContent = 'Ещё нет пинов для Вашего просмотра :с';
                         }
+
+                        if (String(this.args) == 'new') {
+                            createScroll(this.args);
+                        }
                     });
                 });
     }
+}
+
+/**
+ * createScroll
+ * @param args 
+ */
+function createScroll(args) {
+    window.addEventListener("scroll", function(){
+        
+        if (window.location.pathname != '/index/new') {
+            window.removeEventListener("scroll", function() {});
+            return;
+        }
+
+        const block = document.getElementById('index-page:' + args);
+       
+        let contentHeight = block.offsetHeight;      // 1) высота блока контента вместе с границами
+        let yOffset       = window.pageYOffset;      // 2) текущее положение скролбара
+        let window_height = window.innerHeight;      // 3) высота внутренней области окна документа
+        let y             = yOffset + window_height;
+       
+        // если пользователь достиг конца
+        if(y >= contentHeight)
+        {
+            fetchModule.Get({
+                url: BACKEND_ADDRESS + '/pin/list/' + args + '?limit=10&id=9',
+                body: null,
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((responseBody) => {
+                    (<any>window).CSRFtoken = responseBody.csrf_token;
+
+
+                    const indexPage = document.getElementById('index-page:' + args);
+                    if (responseBody.body.pins) {
+                        const pinsIndex = responseBody.body.pins;
+
+                        for (let i = 0; i < pinsIndex.length; i++) {
+                            const pinForIndexView = new PinForIndex(indexPage);
+                            pinForIndexView.render({
+                                id: pinsIndex[i].id,
+                                pinImg: BACKEND_ADDRESS + '/' + pinsIndex[i].pin_dir,
+                                content: pinsIndex[i].title});
+                        }
+                    }
+                });
+        }
+    });
 }
