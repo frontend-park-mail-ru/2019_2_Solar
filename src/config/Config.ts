3791f923f3891b45f5ea26ee1600139fc7911c34
Nell <ADshishova@mail.ru>
2019-12-16 17:25:53 +0300

    for chat

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,4 +1,5 @@
 export const BACKEND_ADDRESS = 'https://solarsunrise.ru/api/v1';
 export const WS_BACKEND_ADDRESS = 'wss://solarsunrise.ru:8080';
+export const WSS_BACKEND_ADDRESS = 'wss://solarsunrise.ru';
 // export const WS_BACKEND_ADDRESS = 'ws://localhost:8080';
 // export const BACKEND_ADDRESS = 'http://localhost:8080';
