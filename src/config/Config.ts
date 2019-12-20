e8d9ea05690189c44861f3a952f8db23fd4fc21f
Bogdan Salyp <bogdansalyp@gmail.com>
2019-12-20 13:43:58 +0000

    nginx fixed

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,5 +1,5 @@
 export const BACKEND_ADDRESS = 'https://solarsunrise.ru/api/v1';
 export const PIN_ADRESS = 'https://solarsunrise.ru';
-export const WS_BACKEND_ADDRESS = 'wss://solarsunrise.ru:8080';
+export const WS_BACKEND_ADDRESS = 'wss://solarsunrise.ru/api/v1';
 // export const WS_BACKEND_ADDRESS = 'ws://localhost:8080';
 // export const BACKEND_ADDRESS = 'http://localhost:8080';
